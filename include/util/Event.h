dbc086d0618101b5aa0cc767777480bdee8d3805
Dannode36 <russelme848@gmail.com>
2024-01-14 14:17:35 +1100

    Parameter Pack Events

--- include/util/Event.h
+++ include/util/Event.h
@@ -2,26 +2,26 @@
 #include <functional>
 #include <vector>
 
-template<typename T>
+template<typename... Ps>
 class Event
 {
-    std::vector<std::function<void(T&)>> m_EventHandlers;
+    std::vector<std::function<void(Ps&...)>> m_EventHandlers;
 public:
-    inline void addListener(std::function<void(T&)> handler) {
+    inline void addListener(std::function<void(Ps&...)> handler) {
         m_EventHandlers.push_back(handler);
     }
 
-    inline void removeListener(std::function<void(T&)> handler) {
+    inline void removeListener(std::function<void(Ps&...)> handler) {
         auto it = std::find(m_EventHandlers.begin(), m_EventHandlers.end(), handler);
         if (it != m_EventHandlers.end()) {
             m_EventHandlers.erase(it);
         }
     }
 
-    inline virtual void fire(T& args) {
+    inline virtual void fire(Ps&... args) {
         for (auto& handler : m_EventHandlers)
         {
-            handler(args);
+            handler(args...);
         }
     }
 };
